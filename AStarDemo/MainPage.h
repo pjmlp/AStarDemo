1e69fe8f9247c1ce295ba3a1d0a38c8a794e6567
pjmlp <pjmlp@progtools.org>
2021-09-04 11:06:46 +0200

    Adding the header comments.

--- AStarDemo/MainPage.h
+++ AStarDemo/MainPage.h
@@ -1,4 +1,27 @@
-﻿#pragma once
+﻿/* MainPage.h - Application main window.
+* Copyright (C) 2020 Paulo Pinto
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+* Boston, MA 02111-1307, USA.
+*/
+
+#ifndef MAINPAGE_H
+#define MAINPAGE_H
+
+
+#pragma once
 
 #include "MainPage.g.h"
 
@@ -28,3 +51,5 @@ namespace winrt::AStarDemo::factory_implementation
     {
     };
 }
+
+#endif /* MAINPAGE_H */
